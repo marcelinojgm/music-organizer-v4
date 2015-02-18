1e3ac33bba41fd112902e433a8650e5901bfcf37
MJGM <MJGM@example.com>
2015-02-18 13:24:14 +0100

    correccion contemplacion de que este files este vacio

--- MusicOrganizer.java
+++ MusicOrganizer.java
@@ -88,19 +88,20 @@ public class MusicOrganizer
         int index = 0;
         // Record that we will be searching until a match is found.
         boolean searching = true;
-
-        do {
-            String filename = files.get(index);
-            if(filename.contains(searchString)) {
-                // A match. We can stop searching.
-                searching = false;
-            }
-            else {
-                // Move on.
-                index++;
-            }
-        }while(searching && index != files.size());
-
+        if( files.size() != 0)
+        {
+            do {
+                String filename = files.get(index);
+                if(filename.contains(searchString)) {
+                    // A match. We can stop searching.
+                    searching = false;
+                }
+                else {
+                    // Move on.
+                    index++;
+                }
+            }while(searching && index != files.size());
+        }
         if(searching) {
             // We didn't find it.
             return -1;
